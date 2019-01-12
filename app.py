18b6bfec6fee4997bab542a7c2786275257620e3
Jonathan Guiang <jkguiang@gmail.com>
2019-01-11 21:50:17 -0800

    added celery and redis

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
+# General
+import os
 # Flask
 from flask import Flask, render_template, flash, request, url_for, jsonify
-import os
+# Celery
+from celery import Celery
 # API
 import api
 
+# Configure app
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 
-def APICall(url="", img="", nColors=6):
-    # Retrieve image
-    if not img:
-        img = api.ParseCSS(url)
-    else:
-        img = api.ProcImage(img)
-    # Shape data
-    data = api.GetData(img)
-    # Run clustering algorithm
-    colors = api.Cluster(nColors, data)
+# Configure Celery
+celery = Celery(
+    app.name,
+    broker=app.config['CELERY_BROKER_URL']
+)
+celery.conf.update(app.config)
 
-    return { "result": { "colors": colors } }
+@celery.task
+def APICall(data={}):
+    """ Asynchronous call to ChompChap API """
+    result = api.Call(data)
+
+    return { "result": result }
 
 @app.route("/", methods=['GET', 'POST'])
 def Index():
+    """ Main page """
     return render_template("index.html")
 
 @app.route("/query", methods=['POST'])
 def Query():
-    url = (request.json).get("url", False)
-    img = (request.json).get("img", False)
-    response = APICall(url=url, img=img)
-
-    return jsonify(response)
+    """ Handle requests """
+    data = request.data
+    print(data)
+    task = APICall.apply_async(args=[data])
+    return jsonify({}), 202, {'Location': url_for('Status', taskID=task.id)}
 
 if __name__ == "__main__":
     app.debug = True
